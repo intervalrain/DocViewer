1b077d8f7e8b57667acc0c45830b2892af988858
Rain Hu <intervalrain@gmail.com>
2024-06-11 01:33:34 +0800

    Impl authorization service and policy enforcer

---
+++ DocViewer.Infrastructure/Security/AuthorizationService.cs
@@ -0,0 +1,45 @@
+﻿using DocViewer.Application.Common.Interfaces;
+using DocViewer.Application.Common.Security.Request;
+using DocViewer.Application.Common.Security.Users;
+
+using ErrorOr;
+
+namespace DocViewer.Infrastructure.Security;
+
+public class AuthorizationService : IAuthorizationService
+{
+    private readonly IPolicyEnforcer _policyEnforcer;
+    private readonly CurrentUser _currentUser;
+
+    public AuthorizationService(IPolicyEnforcer policyEnforcer, ICurrentUserProvider currentUserProvider)
+    {
+        _policyEnforcer = policyEnforcer;
+        _currentUser = currentUserProvider.CurrentUser;
+    }
+
+    public ErrorOr<Success> AuthorizeCurrentUser<T>(IAuthorizeableRequest<T> request, List<string> requiredRoles, List<string> requiredPermissions, List<string> requiredPolicies)
+    {
+        if (requiredRoles.Except(_currentUser.Roles).Any())
+        {
+            return Error.Unauthorized(description: "User is missing required roles for taking this action.");
+        }
+
+        if (requiredPermissions.Except(_currentUser.Permissions).Any())
+        {
+            return Error.Unauthorized(description: "User is missing required permissions for taking this action.");
+        }
+
+        foreach (var policy in requiredPolicies)
+        {
+            var authorizationAgainstPolicyResult = _policyEnforcer.Authorize(request, _currentUser, policy);
+
+            if (authorizationAgainstPolicyResult.IsError)
+            {
+                return authorizationAgainstPolicyResult.Errors;
+            }
+        }
+
+        return Result.Success;
+    }
+}
+
---
+++ DocViewer.Infrastructure/Security/PolicyEnforcer.cs
@@ -0,0 +1,28 @@
+﻿using DocViewer.Application.Common.Interfaces;
+using DocViewer.Application.Common.Security.Request;
+using DocViewer.Application.Common.Security.Users;
+using ErrorOr;
+
+namespace DocViewer.Infrastructure.Security;
+
+public class PolicyEnforcer : IPolicyEnforcer
+{
+    public const string SelfOrAdmin = nameof(SelfOrAdmin);
+    public const string Admin = nameof(Admin);
+
+    public ErrorOr<Success> Authorize<T>(IAuthorizeableRequest<T> request, CurrentUser currentUser, string policy)
+    {
+        return policy switch
+        {
+            SelfOrAdmin => SelfOrAdminPolicy(request, currentUser),
+            _ => Error.Unexpected(description: "Unknown policy name")
+        };
+    }
+
+    private static ErrorOr<Success> SelfOrAdminPolicy<T>(IAuthorizeableRequest<T> request, CurrentUser currentUser)
+    {
+        return request.UserId == currentUser.UserId || currentUser.Roles.Contains(Admin)
+            ? Result.Success
+            : Error.Unauthorized(description: "Requesting user failed policy requirement");
+    }
+}
